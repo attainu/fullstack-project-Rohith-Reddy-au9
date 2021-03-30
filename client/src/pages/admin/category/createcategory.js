379b1997d7f92533d2841aae6bf57007d51bce8f
Rohith Reddy <43477613+Rohith-Reddy-au9@users.noreply.github.com>
2021-03-30 21:58:39 +0530

    final update

--- client/src/pages/admin/category/createcategory.js
+++ client/src/pages/admin/category/createcategory.js
@@ -47,8 +47,7 @@ const CategoryCreate = () => {
     };
 
     const handleRemove = async (slug) => {
-      // let answer = window.confirm("Delete?");
-      // console.log(answer, slug);
+   
       if (window.confirm("Delete?")) {
         setLoading(true);
         removeCategory(slug, user.token)
@@ -65,11 +64,6 @@ const CategoryCreate = () => {
           });
       }
     };
-
-    // const handleSearch = (e) =>{
-    //   e.preventDefault();
-    //   setKeyword(e.target.value.toLowerCase());
-    // }
 
     const searchedItem = (keyword) => (c) => c.name.toLowerCase().includes(keyword);
 
@@ -95,13 +89,6 @@ const CategoryCreate = () => {
               keyword={keyword}
               setKeyword = {setKeyword}
             />
-            {/* <input 
-              type = "search"
-              className="form-control mb-4"
-              value={keyword}
-              placeholder="Search"
-              onChange={handl eSearch}
-            /> */}
             <hr />
             {categories.filter(searchedItem(keyword)).map((c) => (
               <div className="alert alert-secondary" key={c._id}>
@@ -126,119 +113,3 @@ const CategoryCreate = () => {
 };
 
 export default CategoryCreate;
-
-// my code get category error
-
-// const CategoryCreate = () => {
-
-//   const { user } = useSelector((state) => ({...state}))
-
-//   const [name, setName] = useState("")
-//   const [loading, setLoading] = useState(false)
-//   const [categories, setCategories] = useState([])
-
-//   useEffect(() => {
-//     // allcategories =  getCategories().then((c) => setCategories(c.data))
-//     allcategories()
-//   }, [])
-
-//   const allcategories = () => getCategories().then((c) => setCategories(c.data))
-//   console.log(">>>>>",allcategories())
-  
-//   const handleSubmit= (e) => {
-//     e.preventDefault()
-//     // console.log("name>>>>",name)
-//     setLoading(true)
-//     createCategory({name}, user.token)
-//       .then((res) => {
-//         // console.log("res>>>>", res)
-//         setLoading(false)
-//         setName('')
-//         toast.success(`"${res.data.name}" is craeted`)
-//         allcategories()
-//       })
-//       .catch((err) => {
-//         // console.log("er>>>>", err)
-//         setLoading(false)
-//         if(err.response.status === 400) toast.error(err.response.data)
-//       });
-//   }
-
-//   const handleRemove = (slug) =>{
-//     // let ans =(window.confirm(`Are you sure you want to delete ${slug} item`))
-//     // console.log(ans, slug)
-//       if(window.confirm('Are you sure you want to delete')){
-//         setLoading(true)
-//         removeCategory(slug, user.token)
-//         .then((res)=>{
-//           setLoading(false)
-//           toast.error(`successfully you deleted ${res.data.name} item`)
-//           allcategories()
-//         })
-//         .catch((err) => {
-//           if (err.response.status === 400) {
-//             setLoading(false);
-//             toast.error(err.response.data);
-//           }
-//         })
-//       }
-//     }
-  
-
-//   const categoryForm = () => <form onSubmit={handleSubmit}>
-//      <div className="form-group">
-//       <label>Name</label>
-//       <input
-//           type="text"
-//           className="form-control"
-//           onChange={(e) => setName(e.target.value)}
-//           value={name}
-//           autoFocus
-//           required
-//         />
-//         <br />
-//       <button className="btn btn-outline-primary">SAVE</button>
-//      </div>
-//   </form>
-
-
-//   return (
-//       <div className="container-fluid">
-//           <div className="row">
-//             <div className="col-md-2">
-//               <AdminNav />
-//             </div>
-//             <div className="col">
-//               {loading ? (
-//                 <h4 className="text-danger">Loading...</h4>
-//               ):(
-//                 <h4>Category Form</h4> 
-//               )}
-//               {categoryForm()}
-//               <hr />
-              
-//               {categories.map((c) => (
-//                 <div className="alert alert-secondary" key={c._id}>
-//                   {c.name}
-//                   {/* <span
-//                     onClick={() => handleRemove(c.slug)}
-//                     className="btn btn-sm float-right"
-//                   >
-//                     <DeleteOutlined className="text-danger" />
-//                   </span>
-//                   <Link to={`/admin/category/${c.slug}`}>
-//                     <span className="btn btn-sm float-right">
-//                       <EditOutlined className="text-warning" />
-//                     </span>
-//                   </Link> */}
-//                 </div>
-//               ))}
-//           </div>
-//         </div>
-//       </div>
-    
-//   );
-// };
-
-// export default CategoryCreate;
-
